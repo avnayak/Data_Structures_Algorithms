ec3a99e9d826dbec90eab2167beb03f35c60b324
avnayak <akshatavijaynayak@gmail.com>
2018-11-17 17:09:10 -0500

    Age Program implementd

---
+++ age.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import string
+name=raw_input("whats ur name \t \n")
+age=input("whats ur age \t \n")
+print name
+print age
+year100=str((2018-age)+100)
+print "u will complete 100 years at "+year100
+
+
