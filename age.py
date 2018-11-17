4bda4d61e601ea5972f88516d0bae049919ea728
avnayak <akshatavijaynayak@gmail.com>
2018-11-17 17:21:35 -0500

    Age Program implementd

--- age.py
+++ age.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import string
+    # Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
 name=raw_input("whats ur name \t \n")
 age=input("whats ur age \t \n")
 print name
