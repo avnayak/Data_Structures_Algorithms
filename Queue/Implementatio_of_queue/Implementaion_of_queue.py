356e4fc748d54dbd6d20687221b5f92f3940753f
avnayak <akshatavijaynayak@gmail.com>
2019-03-01 18:30:57 -0500

    Simple implementation of queue

--- Queue/Implementatio_of_queue/Implementaion_of_queue.py
+++ Queue/Implementatio_of_queue/Implementaion_of_queue.py
@@ -5,16 +5,20 @@ class queue(object):
     def isEmpty(self):
         return self.items==[]
         
-    def firstIn(self,newElement):
-        return self.items.insert(2,newElement)
+    def firstIn(self,position,newElement):
+        return self.items.insert(position,newElement)
         
     def firstOut(self):
-        return self.items.pop()
+        return self.items.pop(0)
         
 x=queue()
 print x.isEmpty()
-print x.firstIn(4)
-print x.firstIn(78)
+print x.firstIn(1,45)
+print x.firstIn(2,4)
+print x.firstIn(0,78)
 print x.firstOut()
+
+
+
 
 
