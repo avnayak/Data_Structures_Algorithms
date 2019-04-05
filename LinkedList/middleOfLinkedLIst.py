b8c919f4c376a638ad2eb86f83f767f46a2d4a87
avnayak <akshatavijaynayak@gmail.com>
2019-04-05 14:06:48 -0400

    leetcode 876

--- LinkedList/middleOfLinkedLIst.py
+++ LinkedList/middleOfLinkedLIst.py
@@ -2,7 +2,7 @@ class Solution:
 #Using two-pointer approach
 #leetcode 876
     def middleNode(self,head):
-        slow, fast = head
+        slow, fast = head,head
         while fast and fast.next:
             fast=fast.next.next
             slow=slow.next
